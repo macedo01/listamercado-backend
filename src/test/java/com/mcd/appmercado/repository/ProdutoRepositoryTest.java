5bce464e368d5aa989e36b666e831bd06e7355fb
Macedo <pedromacedo113@hotmail.com>
2023-01-06 22:49:30 -0300

    teste

---
+++ src/test/java/com/mcd/appmercado/repository/ProdutoRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.mcd.appmercado.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProdutoRepositoryTest {
+
+    @Test
+    void setupClass() {
+        String nul = "null";
+        assertNotNull(nul);
+    }
+}
